a013f2f199f1751e2dde98e75db48cafd716fa07
Phuong Nguyen <minhphuong15hcb1@gmail.com>
2022-06-07 08:23:37 +0700

    Adding ImageLinkForm component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React from 'react'; //import React, { Component }  from 'react';
-import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import './App.css';
 
 
 function App() {
@@ -9,9 +10,9 @@ function App() {
     <div className="App">
       <Navigation/>
       <Logo/>
+      <ImageLinkForm/>
       {
-        /*
-        <ImageLinkForm/>
+        /*        
         <FaceRecognition/>
         */
       }
